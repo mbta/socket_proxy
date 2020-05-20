d2e95acb7126e8eb9e51f7986c27fef399a25739
Gabe Durazo <gdurazo@mbta.com>
2020-05-20 14:48:16 -0500

    feat: Build application as a release

---
+++ config/prod.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+config :logger, backends: [{Logger.Backend.Splunk, :splunk_log}, :console]
+
+config :logger, :splunk_log,
+  host: "https://http-inputs-mbta.splunkcloud.com/services/collector/event",
+  token: {:system, "SOCKET_PROXY_SPLUNK_TOKEN"},
+  format: "$dateT$time $metadata[$level] node=$node $message\n",
+  level: :info
