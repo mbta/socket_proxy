91c6b93bfb35740295434c375cd12f2b2b9f9c02
Gabe Durazo <gdurazo@mbta.com>
2020-05-20 16:07:25 -0500

    chore: Use DynamicSupervisor and not deprecated :simple_one_for_one

--- lib/socket_proxy/receiver_supervisor.ex
+++ lib/socket_proxy/receiver_supervisor.ex
@@ -1,17 +1,19 @@
 defmodule SocketProxy.ReceiverSupervisor do
-  use Supervisor
+  use DynamicSupervisor
   require Logger
 
   def start_link(arg) do
-    Supervisor.start_link(__MODULE__, arg, name: __MODULE__)
-  end
-
-  def init(_arg) do
-    Supervisor.init([receiver_child_spec()], strategy: :simple_one_for_one)
+    DynamicSupervisor.start_link(__MODULE__, arg, name: __MODULE__)
   end
 
   def start_child(arg) do
-    {:ok, _pid} = Supervisor.start_child(__MODULE__, [arg])
+    spec = %{id: SocketProxy.Receiver, start: {SocketProxy.Receiver, :start_link, [arg]}}
+    {:ok, _pid} = DynamicSupervisor.start_child(__MODULE__, spec)
+  end
+
+  @impl DynamicSupervisor
+  def init(_arg) do
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
 
   def receiver_child_spec do
